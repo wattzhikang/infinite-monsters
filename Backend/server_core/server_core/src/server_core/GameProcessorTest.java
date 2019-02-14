7faf5922f5c94e09a279cf916eb67d657b1348f2
Zachariah Watt <zjwatt@localhost.localdomain>
2019-02-13 21:33:21 -0600

    non-functional junit

--- Backend/server_core/server_core/src/server_core/GameProcessorTest.java
+++ Backend/server_core/server_core/src/server_core/GameProcessorTest.java
@@ -2,13 +2,35 @@ package server_core;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 import org.junit.jupiter.api.Test;
 
 class GameProcessorTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		ServerTester server = new ServerTester();
+		server.start();
+		
+		ObjectOutputStream out = null;
+		try {
+			Socket sendSocket = new Socket("127.0.0.1", ServerCore.PORT);
+			out = new ObjectOutputStream(sendSocket.getOutputStream());
+			out.writeObject("Message Test");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("failure");
+		}
+	}
+	
+	private class ServerTester extends Thread {
+		public void run() {
+			ServerCore.main(null);
+		}
 	}
 
 }
