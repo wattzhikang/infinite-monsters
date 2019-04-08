892ba4401b3c3fa8a12334b66f1c1cbc61df7b27
Zachariah Watt <zjwatt@git.linux.iastate.edu>
2019-04-07 23:43:34 -0500

    created Game class in web application. It doesn't do anything

--- Backend/IntermonReact/intermon/src/index.js
+++ Backend/IntermonReact/intermon/src/index.js
@@ -6,11 +6,23 @@ import * as serviceWorker from './serviceWorker';
 var connection = {
     socket: null,
     queue : [],
+    game : null,
     instantiate : () => {
         if (connection.isOpen) {
             connection.socket = new WebSocket('ws://localhost:8080/websocket/zw');
             connection.socket.onmessage = function(event) {
-                alert("Message received: " + event.data);
+                console.log("Message received: " + event.data);
+                let object = JSON.parse(event.data);
+                if (object.subscriptionID !== undefined 
+                    && object.newDungeon !== undefined
+                    && object.xL !== undefined
+                    && object.xR !== undefined
+                    && object.yU !== undefined
+                    && object.yL !== undefined
+                    && object.tiles !== undefined
+                ) {
+                    connection.game.receivedDeltaFrame(object);
+                }
             };
             connection.socket.onopen = function(event) {
                 for (let string of connection.queue) {
@@ -44,6 +56,10 @@ function Credentials(name, password) {
     this.password = password;
 }
 
+function RequestSubscription() {
+    this.requestType = "subscription";
+}
+
 class Login extends React.Component {
     constructor(props) {
         super(props);
@@ -73,6 +89,9 @@ class Login extends React.Component {
         connection.send(
             JSON.stringify(new Credentials(this.state.name, this.state.password))
         );
+        connection.send(
+            JSON.stringify(new RequestSubscription())
+        );
     }
     
     render() {
@@ -96,7 +115,36 @@ class Login extends React.Component {
     }
 }
 
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        connection.game = this;
+        this.map = null;
+    }
+
+    receivedDeltaFrame(deltaframe) {
+        if (this.map = null) {
+            this.map = Array(
+                (deltaframe.xR - deltaframe.xL)
+                * (deltaframe.yU - deltaframe.yL)
+            ).fill(null);
+        }
+    }
+
+    testShowHistory() {
+
+    }
+
+    render() {
+        return (
+            <h1>Here haveth game</h1>
+        );
+    }
+}
+
 ReactDOM.render(<Login />, document.getElementById('root'));
+
+ReactDOM.render(<Game />, document.getElementById('game'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
